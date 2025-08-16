25f275dffbdc6f6a25b2505045f306555b78d444
mcuste <github@muratcanuste.com>
2025-08-16 16:12:26 +0200

    update homebrew script to check if already installed

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -1,7 +1,18 @@
-#!/usr/bin/env dash
+#!/usr/bin/env bash
 
-# Install homebrew
-/bin/dash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Check if Homebrew is installed
+if ! command -v brew >/dev/null 2>&1; then
+	echo "Homebrew not found. Installing..."
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	echo "Homebrew is already installed."
+fi
 
-# Run after homebrew install to set path properly
-sudo launchctl config user path "$(brew --prefix)/bin:${PATH}
+# Set PATH for Homebrew if not already set
+BREW_PATH="$(brew --prefix)/bin"
+if [[ ":$PATH:" != *":$BREW_PATH:"* ]]; then
+	echo "Setting Homebrew path with launchctl."
+	sudo launchctl config user path "$BREW_PATH:${PATH}"
+else
+	echo "Homebrew path already in PATH."
+fi
