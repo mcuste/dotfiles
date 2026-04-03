ad2dd2f555f0be18c2fefe52930ddd79dbc4fbae
mcuste <github@muratcanuste.com>
2026-04-03 20:06:58 +0200

    fix smart splits pane movement

--- stow/.config/nvim/lua/config/keymaps.lua
+++ stow/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Override LazyVim's default <C-h/j/k/l> window navigation with smart-splits
+-- (LazyVim maps these to <C-w>h/j/k/l which doesn't cross tmux pane boundaries)
+vim.keymap.set("n", "<C-h>", "<cmd>SmartCursorMoveLeft<cr>", { desc = "Move to left split" })
+vim.keymap.set("n", "<C-j>", "<cmd>SmartCursorMoveDown<cr>", { desc = "Move to split below" })
+vim.keymap.set("n", "<C-k>", "<cmd>SmartCursorMoveUp<cr>", { desc = "Move to split above" })
+vim.keymap.set("n", "<C-l>", "<cmd>SmartCursorMoveRight<cr>", { desc = "Move to right split" })
