58376b9e95392f6bf3df64a31b81321471922053
mcuste <github@muratcanuste.com>
2026-07-21 19:43:32 +0200

    add herdr plugin installation script

---
+++ stow/.config/herdr/install-plugins.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+herdr plugin install fullerzz/herdr-plugin-sesh --yes
+herdr plugin install lmilojevicc/herdr-splits.nvim --yes
+herdr plugin install persiyanov/herdr-reviewr --yes
