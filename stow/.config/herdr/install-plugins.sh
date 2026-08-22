060f75c67dfe8b0332f9da16577d1d683376d93c
mcuste <github@muratcanuste.com>
2026-08-22 22:51:51 +0200

    setup herdr-workspacer to herdr config

--- stow/.config/herdr/install-plugins.sh
+++ stow/.config/herdr/install-plugins.sh
@@ -4,9 +4,11 @@ set -euo pipefail
 herdr integration install omp
 herdr plugin install lmilojevicc/herdr-splits.nvim --yes
 herdr plugin install persiyanov/herdr-reviewr --yes
+herdr plugin install mcuste/herdr-workspacer --yes
 
 herdr plugin enable herdr-splits
 herdr plugin enable persiyanov.reviewr
+herdr plugin enable herdr-workspacer
 
 herdr update --handoff
 herdr server reload-config
