f310cfd701209ed32243b7f801151ef5c8f4f92e
mcuste <github@muratcanuste.com>
2026-08-22 11:16:16 +0200

    add omp herdr integration to herdr setup script

--- stow/.config/herdr/install-plugins.sh
+++ stow/.config/herdr/install-plugins.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+herdr integration install omp
 herdr plugin install fullerzz/herdr-plugin-sesh --yes
 herdr plugin install lmilojevicc/herdr-splits.nvim --yes
 herdr plugin install persiyanov/herdr-reviewr --yes
