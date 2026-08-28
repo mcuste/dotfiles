06be4bc92e24cf85f77ca9cf0b2e484972f31385
mcuste <github@muratcanuste.com>
2026-08-28 21:15:33 +0200

    fix herdr-context.nvim keybinds/setup

--- stow/.config/nvim/plugin/48_herdr_context.lua
+++ stow/.config/nvim/plugin/48_herdr_context.lua
@@ -1,16 +1,26 @@
 if vim.env.HERDR_ENV ~= '1' then return end
 
 Config.now(function()
-  vim.pack.add({
-    { src = 'https://github.com/mcuste/herdr-context.nvim', version = vim.version.range('^1') },
-  })
+  vim.pack.add({ 'https://github.com/mcuste/herdr-context.nvim' })
 
   require('herdr-context').setup({
     mappings = {
-      buffer = '<Leader>ab',
-      buffers = '<Leader>aB',
-      diagnostics = '<Leader>ad',
-      buffers_diagnostics = '<Leader>aD',
+      buffer = '<leader>aa',
+      buffers = '<leader>aA',
+      diagnostics = '<leader>ad',
+      buffers_diagnostics = '<leader>aD',
+      messages = '<leader>am',
+      quickfix = '<leader>aq',
+      quickfix_all = '<leader>aQ',
+      loclist = '<leader>al',
     },
+    quickfix = {
+      limit = 50,
+    },
+  })
+
+  vim.list_extend(Config.leader_group_clues, {
+    { mode = 'n', keys = '<Leader>a', desc = '+Agent' },
+    { mode = 'x', keys = '<Leader>a', desc = '+Agent' },
   })
 end)
